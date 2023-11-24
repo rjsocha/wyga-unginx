bde655f4629d80f78593535ff3c35dfaaf4aad1f
Robert Socha <socha@socha.it>
2023-11-25 00:36:36 +0100

    unginx

--- context/entrypoint.sh
+++ context/entrypoint.sh
@@ -2,5 +2,5 @@
 set -eu
 configus-config
 auth-config
-exec /entrypoint/nginx-slim "$@" || printf -- "::ERROR: chain loading error\n"
+exec /.entrypoint/nginx-slim "$@" || printf -- "::ERROR: chain loading error\n"
 exit 1
