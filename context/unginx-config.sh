6af3e8c7a1524e08f3c6670b5c314a8822941323
Robert Socha <socha@socha.it>
2024-09-01 04:03:08 +0200

    unginx

--- context/unginx-config.sh
+++ context/unginx-config.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 set -eu
-if [ -n "${CONFIGUS:-}" ]
-then
+if [ -n "${CONFIGUS:-}" ]; then
   export CONFIGUS_ENDPOINT="${CONFIGUS_ENDPOINT:-configus}"
   printf -- "UNGINX RUNTIME: 1.0.0 (%s/%s) CONFIGUS: %s\n" "$(id -u)" "$(id -g)" "${CONFIGUS}" >&2 || true
   exec config-service "${CONFIGUS}"
+fi
+if [ -n "${UNGINX_PROFILE:-}" ]; then
+  printf -- "PROFILE: %s\n" "${UNGINX_PROFILE}"
 fi
