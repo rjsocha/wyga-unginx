0a5de5b7a1cfea539ebd455e5b143b6bb69194ac
Robert Socha <robert.s@deligo.pl>
2025-05-10 13:58:34 +0200

    unginx

--- context/unginx-config.sh
+++ context/unginx-config.sh
@@ -5,11 +5,10 @@ if [ -n "${CONFIGUS:-}" ]; then
   printf -- "UNGINX RUNTIME: 1.0.0 (%s/%s) CONFIGUS: %s\n" "$(id -u)" "$(id -g)" "${CONFIGUS}" >&2 || true
   exec config-service "${CONFIGUS}"
 fi
-if [ -n "${UNGINX_PROFILE:-}" ]; then
-  if [ -f "/config/nginx/profile/${UNGINX_PROFILE}" ]; then
-    printf -- "PROFILE: %s\n" "${UNGINX_PROFILE}"
-    cp -- "/config/nginx/profile/${UNGINX_PROFILE}" "/config/nginx/site/default"
+if [ -n "${PROFILE:-}" ]; then
+  if [ -f "/config/nginx/profile/${PROFILE}" ]; then
+    cp -- "/config/nginx/profile/${PROFILE}" "/config/nginx/site/default"
   else
-    printf -- "PROFILE: <MISSING>%s\n"
+    printf -- "PROFILE NOT FOUND: %s\n" "${PROFILE}"
   fi
 fi
