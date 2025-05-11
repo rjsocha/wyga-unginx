748c9d9f519dce8a37b7673b8035e21e2f46d034
Robert Socha <robert.s@deligo.pl>
2025-05-11 17:26:50 +0200

    unginx

--- context/entrypoint.sh
+++ context/entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 set -eu
+if [ -n "${1-}" ]; then
+  if [ "${1::1}" = "@" ]; then
+    cmd="${1:1}"
+    shift
+    exec "${cmd}" "${@}"
+  fi
+fi
 home-config
 unginx-config
 auth-config
--- context/unginx-config.sh
+++ context/unginx-config.sh
@@ -5,10 +5,11 @@ if [ -n "${CONFIGUS:-}" ]; then
   printf -- "UNGINX RUNTIME: 1.0.0 (%s/%s) CONFIGUS: %s\n" "$(id -u)" "$(id -g)" "${CONFIGUS}" >&2 || true
   exec config-service "${CONFIGUS}"
 fi
-if [ -n "${PROFILE:-}" ]; then
-  if [ -f "/config/nginx/profile/${PROFILE}" ]; then
-    cp -- "/config/nginx/profile/${PROFILE}" "/config/nginx/site/default"
-  else
-    printf -- "PROFILE NOT FOUND: %s\n" "${PROFILE}"
+if [ -n "${PROFILE-}" ] && [ "${PROFILE-}" != "none" ]; then
+  if ! template-engine "${PROFILE}" "/config/nginx/site/default"; then
+    rc=$?
+    echo RC: $rc
+    printf -- "ERROR: unable to render profile: %s\n" "${PROFILE}" >&2
+    exit 100
   fi
 fi
